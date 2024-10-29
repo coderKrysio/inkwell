4995b54a2f17c12de6cd58965afdc02dc46e1994
Anushree <anushreejaiswal.srn@gmail.com>
2024-10-30 01:49:47 +0530

    updated styles and added post cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { PostCard } from "@/components/post-card";
+
 export default function Home() {
     return (
-        <main className="flex items-center justify-center min-h-screen p-8 pb-20">
-            Inkwell
+        <main className="flex items-start justify-center w-full min-h-screen p-14 pt-[92px]">
+            <div className="flex flex-wrap items-center justify-start gap-14 w-full">
+                <PostCard />
+                <PostCard />
+                <PostCard />
+            </div>
         </main>
     );
 }
