3a2988da4ab4c06d3228fb8f559275f17de9f4f8
Anushree <anushreejaiswal.srn@gmail.com>
2024-10-30 23:24:22 +0530

    updated page styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,10 @@ import { PostCard } from "@/components/post-card";
 
 export default function Home() {
     return (
-        <main className="flex items-start justify-center w-full min-h-screen p-14 pt-[92px]">
-            <div className="flex flex-wrap items-center justify-start gap-14 w-full">
-                <PostCard />
-                <PostCard />
-                <PostCard />
-            </div>
-        </main>
+        <div className="flex flex-wrap items-center justify-start gap-14 w-full max-[768px]:justify-center">
+            <PostCard />
+            <PostCard />
+            <PostCard />
+        </div>
     );
 }
