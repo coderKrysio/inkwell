b18e0eaa3692ff57b318dc06140d0bea003887c8
Anushree <anushreejaiswal.srn@gmail.com>
2024-10-31 00:39:40 +0530

    created detailed blog page

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { ButtonAction } from "@/components/buttonaction";
+
+export default function Page() {
+    return (
+        <div className="w-full flex flex-col gap-4">
+            <h1 className="text-3xl font-bold mt-4 mb-8">Post One</h1>
+            <ButtonAction />
+            <p className="text-slate-700">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Facere, deleniti? Laboriosam, unde officiis velit quisquam
+                exercitationem natus, quam repudiandae eos incidunt ratione quo
+                ipsa alias consequatur ipsum fuga dolor tenetur.
+            </p>
+        </div>
+    );
+}
