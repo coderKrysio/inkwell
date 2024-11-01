3b49bc91465860210293aeec6c95b38a13646564
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-01 23:57:42 +0530

    integrated post api endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import { PostCard } from "@/components/post-card";
+import { db } from "@/lib/db";
+import { title } from "process";
 
-export default function Home() {
+async function getPosts() {
+    const response = await db.post.findMany({
+        select: {
+            id: true,
+            title: true,
+            content: true,
+            tag: true,
+        },
+    });
+    return response;
+}
+
+export default async function Home() {
+    const posts = await getPosts();
     return (
         <div className="flex flex-wrap items-center justify-start gap-14 w-full max-[768px]:justify-center">
-            <PostCard />
-            <PostCard />
-            <PostCard />
+            {posts.map((post) => (
+                <PostCard key={post.id} post={post} />
+            ))}
         </div>
     );
 }
