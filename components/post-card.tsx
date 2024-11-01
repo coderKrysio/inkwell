6c9d49765624cf654a280813ec8ed6d0a9ce1f0f
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-01 23:58:07 +0530

    updated post card information to real time

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -1,18 +1,32 @@
+import { Tag } from "@prisma/client";
 import Link from "next/link";
+import { FC } from "react";
+interface PostCardProps {
+    post: {
+        id: string;
+        title: string;
+        content: string;
+        tag: Tag;
+    };
+}
 
-export const PostCard = () => {
+export const PostCard: FC<PostCardProps> = ({ post }) => {
+    const { id, title, content, tag } = post;
     return (
         <div className="w-[300px] h-[200px] border-2 border-slate-200 rounded-xl shadow-xl p-6 flex flex-col justify-between">
-            <h2 className="text-2xl font-semibold line-clamp-1">Card Title</h2>
-            <p className="line-clamp-3 text-slate-800">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-                molestiae repellat minus nesciunt modi atque quo nemo, facere
-                libero minima quaerat, laboriosam quis sed dolorum ad doloremque
-                voluptatibus rem repudiandae!
-            </p>
-            <Link href={"/blog/1"} className="text-right hover:underline">
-                Read More
-            </Link>
+            <h2 className="text-2xl font-semibold line-clamp-1">{title}</h2>
+            <p className="line-clamp-3 text-slate-800">{content}</p>
+            <div className="flex gap-2 items-center justify-end">
+                <p className="bg-black text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full w-fit">
+                    {tag.name}
+                </p>
+                <Link
+                    href={`/blog/${id}`}
+                    className="text-right hover:underline"
+                >
+                    Read More
+                </Link>
+            </div>
         </div>
     );
 };
