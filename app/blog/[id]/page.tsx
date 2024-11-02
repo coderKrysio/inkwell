b8986e00d430a77c0b1e42cd3a3867a4ae9128de
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-02 16:38:34 +0530

    integrated api endpoint successfully in detailed blog page

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,18 +1,39 @@
 import { BackButton } from "@/components/backbutton";
 import { ButtonAction } from "@/components/buttonaction";
+import { db } from "@/lib/db";
 
-export default function Page() {
+interface BlogPageProps {
+    params: {
+        id: string;
+    };
+}
+
+async function getPost(id: string) {
+    const response = await db.post.findFirst({
+        where: {
+            id: id,
+        },
+        select: {
+            id: true,
+            title: true,
+            content: true,
+            tag: true,
+        },
+    });
+    return response;
+}
+export default async function Page({ params }: BlogPageProps) {
+    const post = await getPost(params.id);
+
     return (
         <div className="w-full flex flex-col gap-4">
             <BackButton />
-            <h1 className="text-3xl font-bold mt-4 mb-8">Post One</h1>
+            <h1 className="text-3xl font-bold mt-4 mb-8">{post?.title}</h1>
             <ButtonAction />
-            <p className="text-slate-700">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Facere, deleniti? Laboriosam, unde officiis velit quisquam
-                exercitationem natus, quam repudiandae eos incidunt ratione quo
-                ipsa alias consequatur ipsum fuga dolor tenetur.
+            <p className="bg-black text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full w-fit">
+                {post?.tag.name}
             </p>
+            <p className="text-slate-700">{post?.content}</p>
         </div>
     );
 }
