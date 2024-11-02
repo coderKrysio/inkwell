a2d8c617b4a2b7ac628c61f54c6d3244e3a097a2
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-02 18:55:42 +0530

    integrated update and delete api endpoints

--- components/buttonaction.tsx
+++ components/buttonaction.tsx
@@ -1,20 +1,51 @@
+"use client";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import { Pencil, Trash } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { FC } from "react";
 
-export const ButtonAction = () => {
+interface ButtonActionProps {
+    id: string;
+}
+export const ButtonAction: FC<ButtonActionProps> = ({ id }) => {
+    const router = useRouter();
+
+    const { mutate: deletePost, isPending } = useMutation({
+        mutationFn: async () => {
+            return axios.delete(`/api/posts/${id}`);
+        },
+        onError: (error) => {
+            console.error(error);
+        },
+        onSuccess: () => {
+            router.push("/");
+            router.refresh();
+        },
+    });
+
     return (
         <div className="mb-2 flex gap-4">
             <Link
-                href={"/edit/1"}
+                href={`/edit/${id}`}
                 className="py-2 px-7 bg-slate-300 font-semibold text-lg rounded-xl flex items-center hover:text-white hover:bg-slate-700 transition-colors"
             >
                 <Pencil width={18} className="mr-2" />
                 Edit
             </Link>
-            <button className="py-2 px-7 bg-red-500 font-semibold text-lg rounded-xl flex items-center text-white hover:bg-red-700 transition-colors">
-                <Trash width={18} className="mr-2" />
-                Delete
+            <button
+                onClick={() => deletePost()}
+                className="py-2 px-7 bg-red-500 font-semibold text-lg rounded-xl flex items-center text-white hover:bg-red-700 transition-colors"
+            >
+                {isPending ? (
+                    "loading..."
+                ) : (
+                    <>
+                        <Trash width={18} className="mr-2" />
+                        Delete
+                    </>
+                )}
             </button>
         </div>
     );
