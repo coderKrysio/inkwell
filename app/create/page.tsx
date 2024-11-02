dbd530dc01389a2a9c0bc6ed7c50efe7712313a3
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-02 18:58:07 +0530

    integrating post api endpoint

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -2,17 +2,38 @@
 import { BackButton } from "@/components/backbutton";
 import { FormPost } from "@/components/formpost";
 import { FormInputPost } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import { SubmitHandler } from "react-hook-form";
 
 export default function Page() {
+    const router = useRouter();
     const handleCreatePost: SubmitHandler<FormInputPost> = (data) => {
-        console.log(data);
+        createPost(data);
     };
+
+    const { mutate: createPost, isPending } = useMutation({
+        mutationFn: (newPost: FormInputPost) => {
+            return axios.post("/api/posts/create", newPost);
+        },
+        onError: (error) => {
+            console.error(error);
+        },
+        onSuccess: () => {
+            router.push("/");
+            router.refresh();
+        },
+    });
     return (
         <div className="w-full flex flex-col items-center gap-4">
             <BackButton />
             <h1 className="text-3xl font-bold text-slate-900">Add new post</h1>
-            <FormPost submit={handleCreatePost} isEditing={false} />
+            <FormPost
+                submit={handleCreatePost}
+                isEditing={false}
+                isPending={isPending}
+            />
         </div>
     );
 }
