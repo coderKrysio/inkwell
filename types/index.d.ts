88ca0e8adb367149243f22a98258ba5720694811
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-04 19:14:04 +0530

    added user type

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,12 @@ export type FormInputPost = {
     title: string;
     content: string;
     tagId: string;
+    authorId: string;
+};
+
+export type User = {
+    first_name: string;
+    last_name: string;
+    email: string;
+    profile_image: string;
 };
