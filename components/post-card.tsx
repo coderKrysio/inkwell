94ed3a3b5b871d794ad9323abd25ad444e7b05d0
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-04 19:19:31 +0530

    added estimated read time and author fields

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -1,4 +1,5 @@
-import { Tag } from "@prisma/client";
+import { wordCounter, wordsPerMinute } from "@/lib/blog/estimateTime";
+import { Tag, User } from "@prisma/client";
 import Link from "next/link";
 import { FC } from "react";
 interface PostCardProps {
@@ -7,14 +8,24 @@ interface PostCardProps {
         title: string;
         content: string;
         tag: Tag;
+        author: User;
     };
 }
 
 export const PostCard: FC<PostCardProps> = ({ post }) => {
-    const { id, title, content, tag } = post;
+    const { id, title, content, tag, author } = post;
+    const postTime = Math.ceil(wordCounter(content) / wordsPerMinute);
     return (
-        <div className="w-[300px] max-h-[200px] border-2 border-slate-200 rounded-xl shadow-xl p-6 flex flex-col gap-2 justify-between">
-            <h2 className="text-2xl font-semibold line-clamp-1">{title}</h2>
+        <div className="w-[300px] h-[200px] border-2 border-slate-200 rounded-xl shadow-xl p-6 flex flex-col gap-2 justify-between">
+            <div className="flex flex-col">
+                <h2 className="text-2xl font-semibold line-clamp-1">{title}</h2>
+                <span className="text-slate-600">
+                    {author.first_name + " " + author.last_name}
+                </span>
+                <span className="text-slate-600">
+                    {postTime} {postTime > 1 ? "mins" : "min"}
+                </span>
+            </div>
             <p className="line-clamp-3 text-slate-800">{content}</p>
             <div className="flex gap-2 items-center justify-end">
                 <p className="bg-black text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full w-fit">
