62e6a26173900f347731e4fbd0e1eef28485ee23
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-13 20:49:08 +0530

    created universal types

---
+++ lib/types/types.ts
@@ -0,0 +1,28 @@
+export interface PageProps {
+    searchParams?: Promise<{
+        search?: string;
+        rd?: string;
+    }>;
+}
+
+export interface PageInfoProps {
+    posts: {
+        id: string;
+        title: string;
+        content: string;
+        tag: {
+            id: string;
+            name: string;
+        };
+        author: {
+            id: string;
+            first_name: string;
+            last_name: string;
+            email: string;
+            profile_image: string | null;
+            created_at: Date;
+            updated_at: Date;
+        };
+    }[];
+    resultLength: number;
+}
