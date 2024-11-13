5aaf43b2b6ff91382473e495c8b65d83c41c3f40
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-13 20:56:20 +0530

    created readTime tags

---
+++ lib/search.ts
@@ -0,0 +1,22 @@
+import { useRouter } from "next/navigation";
+
+const PAGE_SIZE = 2;
+
+export const readTimes = [
+    {
+        name: "< 5 min",
+        value: "4",
+    },
+    {
+        name: "5 min",
+        value: "5",
+    },
+    {
+        name: "10 min",
+        value: "10",
+    },
+    {
+        name: "20+ min",
+        value: "20",
+    },
+];
