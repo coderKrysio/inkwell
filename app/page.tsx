010d9093739680352e04275caba0008d4b93f35a
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-17 19:47:11 +0530

    added custom hook to fetch filtered posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { FilterBar } from "@/components/filter-bar";
 import { PostCard } from "@/components/post-card";
 import { getPageInfo } from "@/lib/hooks/getPageInfo";
-import { PageProps } from "@/lib/types/types";
+import { PageProps } from "@/types";
 
 export default async function Home({ searchParams }: PageProps) {
-    const { posts, resultLength } = await getPageInfo({ searchParams });
+    const { posts, resultLength, tags } = await getPageInfo({ searchParams });
+
     return (
         <div className="w-full flex flex-col gap-4">
-            <FilterBar resultLength={resultLength} />
+            <FilterBar searhtags={tags} resultLength={resultLength} />
             <div className="flex flex-wrap items-center justify-start gap-14 w-full max-[768px]:justify-center">
                 {posts.map((post: any) => (
                     <PostCard key={post.id} post={post} />
