7d6cfcfb79e6ce35f3bbfb9bb619e7c58079f485
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-17 19:43:56 +0530

    updated universal types and interfaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,3 +11,38 @@ export type User = {
     email: string;
     profile_image: string;
 };
+export interface TagsProps {
+    searhtags?: string[];
+    handleReadTimeToggle: (readTime: string) => void;
+    handleTagToggle: (tag: string) => void;
+}
+
+export interface PageProps {
+    searchParams?: Promise<{
+        search?: string;
+        rd?: string;
+        tag?: string[];
+    }>;
+}
+
+export interface PageInfoProps {
+    posts: {
+        id: string;
+        title: string;
+        content: string;
+        tag: {
+            id: string;
+            name: string;
+        };
+        author: {
+            id: string;
+            first_name: string;
+            last_name: string;
+            email: string;
+            profile_image: string | null;
+            created_at: Date;
+            updated_at: Date;
+        };
+    }[];
+    resultLength: number;
+}
