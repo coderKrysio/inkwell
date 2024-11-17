493caa71e8b801d1e5ae986ea48a9200898e6ef7
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-17 14:59:46 +0530

    made search bar compatible with routing

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -1,36 +1,35 @@
 "use client";
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
 import { Input } from "./ui/input";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useDebouncedCallback } from "use-debounce";
+import { Button } from "./ui/button";
+import { useSearchBar } from "@/lib/hooks/useSearchBar";
 
 export const SearchBar = () => {
-    const { replace } = useRouter();
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
-
-    const handleChange = useDebouncedCallback((term: string) => {
-        const params = new URLSearchParams(searchParams);
-        if (term) {
-            params.set("search", term);
-        } else {
-            params.delete("search");
-        }
-        replace(`${pathname}?${params.toString()}`);
-    }, 300);
+    const { value, handleChange, handleClear } = useSearchBar();
 
     return (
         <div className="relative flex-grow min-w-[200px]">
-            <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <Search className="absolute left-2 top-1/2 h-5 w-5 transform -translate-y-1/2 text-gray-400" />
             <Input
-                type="search"
-                placeholder="Search blog posts..."
-                className="pl-10"
+                type="text"
+                className="pr-10 pl-10"
                 onChange={(e) => {
                     handleChange(e.target.value);
                 }}
-                defaultValue={searchParams.get("query")?.toString()}
+                value={value}
+                placeholder="Search blog posts..."
             />
+
+            {value && (
+                <Button
+                    variant="ghost"
+                    onClick={handleClear}
+                    className="absolute inset-y-0 right-0 flex items-center h-full px-2 py-0 hover:bg-transparent"
+                >
+                    <X className="h-4 w-4 text-muted-foreground" />
+                    <span className="sr-only">Clear search</span>
+                </Button>
+            )}
         </div>
     );
 };
