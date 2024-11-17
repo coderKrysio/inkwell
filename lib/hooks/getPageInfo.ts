0021541ba7692a43fda38c84ed0951c978aa9328
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-17 19:46:27 +0530

    updated function calling with tags

--- lib/hooks/getPageInfo.ts
+++ lib/hooks/getPageInfo.ts
@@ -1,11 +1,14 @@
-import { filterPostsByReadTime } from "@/services/getAllPosts";
-import { PageProps } from "../types/types";
+import { getPosts } from "@/services/getAllPosts";
+import { PageProps } from "@/types";
 
 export async function getPageInfo({ searchParams }: PageProps) {
     const search = (await searchParams)?.search as string;
     const rd = parseInt((await searchParams)?.rd as string);
-    const posts = await filterPostsByReadTime({ search, rd });
+    const tagsList = (await searchParams)?.tag;
+    const tags =
+        typeof tagsList === "string" ? [tagsList] : (tagsList as string[]);
+    const posts = await getPosts({ search, rd, tags });
     const resultLength = await posts.length;
 
-    return { posts, resultLength };
+    return { posts, resultLength, tags };
 }
