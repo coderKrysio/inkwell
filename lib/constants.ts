5be25add9f1be48c3b2f6c5ba09bdb109b2aa47d
Anushree <anushreejaiswal.srn@gmail.com>
2024-12-21 00:33:20 +0530

    added constants

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+import { BookOpen, FileEdit, MessageSquare } from "lucide-react";
+
+export const tabs = [
+    { name: "Published", icon: BookOpen },
+    { name: "Drafts", icon: FileEdit },
+    { name: "Comments", icon: MessageSquare },
+];
