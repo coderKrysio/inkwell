703b93bb2b31a3f6d95b40990d16583cb21faab7
Anushree <anushreejaiswal.srn@gmail.com>
2024-12-21 00:24:37 +0530

    updated access to edit blogs

--- app/(protected)/blog/[id]/page.tsx
+++ app/(protected)/blog/[id]/page.tsx
@@ -2,6 +2,7 @@ import { BackButton } from "@/components/backbutton";
 import { ButtonAction } from "@/components/buttonaction";
 import { wordCounter, wordsPerMinute } from "@/lib/blog/estimateTime";
 import { db } from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
 
 async function getPost(id: string) {
@@ -28,18 +29,20 @@ export default async function Page({
     const { id } = await params;
     const post = await getPost(id);
     const postTime = Math.ceil(wordCounter(post?.content) / wordsPerMinute);
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    const userName = user.given_name + " " + user.family_name;
+    const authorName = post?.author.first_name + " " + post?.author.last_name;
 
     return (
         <div className="w-full flex flex-col gap-4">
             <BackButton />
             <h1 className="text-3xl font-bold mt-4 mb-8">{post?.title}</h1>
-            <span className="text-slate-600">
-                {post?.author.first_name + " " + post?.author.last_name}
-            </span>
+            <span className="text-slate-600">{authorName}</span>
             <span className="text-slate-600">
                 {postTime} {postTime > 1 ? "mins" : "min"}
             </span>
-            <ButtonAction id={id} />
+            {authorName === userName && <ButtonAction id={id} />}
             <p className="bg-black text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full w-fit">
                 {post?.tag.name}
             </p>
