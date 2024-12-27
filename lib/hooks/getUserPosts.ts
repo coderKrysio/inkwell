ee28054bc7647d0ef01b7b17e80884fb55d31eff
Anushree <anushreejaiswal.srn@gmail.com>
2024-12-28 00:22:59 +0530

    updated props

--- lib/hooks/getUserPosts.ts
+++ lib/hooks/getUserPosts.ts
@@ -5,11 +5,13 @@ export async function getUserPosts({
     user_id,
     first_name,
     last_name,
+    type,
 }: GetUserPostsProps) {
     const posts = await getUserAllPosts({
         user_id,
         first_name,
         last_name,
+        type,
     });
     const resultLength = await posts.length;
 
