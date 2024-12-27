1cf52ce1d7c58a7a947bbced984d37cd9babdc62
Anushree <anushreejaiswal.srn@gmail.com>
2024-12-28 00:19:22 +0530

    updated landing page styling

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,18 @@ export default async function Home({ searchParams }: PageProps) {
     const { posts, resultLength, tags } = await getPageInfo({ searchParams });
 
     return (
-        <div className="w-full min-h-screen bg-background">
+        <div className="max-w-6xl w-full min-h-screen bg-background">
             {/* To Do: Recommendation Bar */}
             {/* <TopicsNav /> */}
             <FilterBar searhtags={tags} resultLength={resultLength} />
-            <div className="max-w-7xl h-full container mx-auto mt-4 px-4 max-[640px]:px-2">
-                <div className="h-full grid grid-cols-1 gap-8 lg:grid-cols-[1fr_300px]">
-                    <main className="">
+            <div className="w-full h-full container mx-auto mt-6 max-[640px]:px-2">
+                <div className="h-full grid grid-cols-1 gap-16 lg:grid-cols-[1fr_300px]">
+                    <main className="flex flex-col gap-10">
                         {posts.map((post: any) => (
                             <BlogCard key={post.id} post={post} />
                         ))}
                     </main>
-                    <aside className="h-full pl-8 border-l max-[1024px]:border-none max-[1024px]:pl-0">
+                    <aside className="w-[300px] min-w-full h-full pl-10 border-l max-[1024px]:border-none max-[1024px]:pl-0">
                         <RecommendationSection />
                     </aside>
                 </div>
